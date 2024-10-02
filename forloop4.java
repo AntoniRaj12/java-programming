390a16aea76060797df7b9e68bac44ffdf1aa4d1
AntoniRaj12 <144004569+AntoniRaj12@users.noreply.github.com>
2024-10-02 19:31:00 +0530

    Add files via upload

---
+++ forloop4.java
@@ -0,0 +1,13 @@
+class forloop4
+{
+	public static void main(String[] args)
+	{	
+		for (int x=71;x>=51;x--)
+		{
+			if(x%2==0)
+			{				
+			System.out.println(x);
+			}
+		}
+	}
+}
---
+++ sumodd.java
@@ -0,0 +1,16 @@
+class sumodd
+{
+	public static void main(String[] args)
+	{
+		int sum=0;
+		
+		for (int i=71;i<=101;i++)
+		{
+			if(i%2!=0)
+			{
+			sum=sum+i;
+			}
+		}
+		System.out.println(sum);
+	}
+}
