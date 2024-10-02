d7049e3c9acc92d8694a5941026f4b469aabdf2f
AntoniRaj12 <144004569+AntoniRaj12@users.noreply.github.com>
2024-10-02 19:35:05 +0530

    Add files via upload

---
+++ whiletask5.java
@@ -0,0 +1,15 @@
+class whiletask5
+{
+	public static void main(String[] args)
+	{	
+		int i=212;
+		while (i<=415)
+		{
+			if(i%2!=0)
+			{
+			System.out.println(i);
+			}
+			i++;
+		}
+	}
+}
---
+++ whiletask6.java
@@ -0,0 +1,14 @@
+class whiletask6
+{
+	public static void main(String[] args)
+	{	
+		int i=55;
+		int sum=0;
+		while (i<=99)
+		{
+			sum=sum+i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+}
